9889c1cfb5fe4b14b01875ac1358f2159c708782
Batyrkhan Tursumbekov <tumarbatr@gmail.com>
2025-10-23 01:11:26 +0500

    Add basic player movement, collision

---
+++ src/player/player.gd
@@ -0,0 +1,73 @@
+extends CharacterBody2D
+
+
+@export var gravity := 750
+@export var run_speed := 150
+@export var jump_speed := -300
+
+enum { IDLE, RUN, JUMP, HURT, DEAD }
+
+var state := IDLE
+
+func _ready() -> void:
+	change_state(IDLE)
+
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity * delta
+	get_input()
+	move_and_slide()
+	if state == JUMP and is_on_floor():
+		change_state(IDLE)
+	if state == JUMP and velocity.y > 0:
+		$AnimationPlayer.play("jump_down")
+
+func reset(_position) -> void:
+	position = _position
+	show()
+	change_state(IDLE)
+
+func change_state(new_state) -> void:
+	state = new_state
+	match state:
+		IDLE:
+			$AnimationPlayer.play("idle")
+		RUN:
+			$AnimationPlayer.play("run")
+		HURT:
+			$AnimationPlayer.play("hurt")
+		JUMP:
+			$AnimationPlayer.play("jump_up")
+		DEAD:
+			hide()
+
+
+func get_input() -> void:
+	var right = Input.is_action_pressed("right")
+	var left = Input.is_action_pressed("left")
+	var jump = Input.is_action_just_pressed("jump")
+	
+	#Movement occurs in all states
+	velocity.x = 0
+	if right:
+		velocity.x += run_speed
+		$Sprite2D.flip_h = false
+	if left:
+		velocity.x -= run_speed
+		$Sprite2D.flip_h = true
+	
+	#Allow jump only if on the ground
+	if jump and is_on_floor():
+		change_state(JUMP)
+		velocity.y = jump_speed
+	
+	#Transiton to the RUN state when start moving
+	if state == IDLE and velocity.x != 0:
+		change_state(RUN)
+	
+	#Transition to IDLE when standing still
+	if state == RUN and velocity.x == 0:
+		change_state(IDLE)
+	
+	#Transition to JUMP when in air
+	if state in [IDLE, RUN] and !is_on_floor():
+		change_state(JUMP)
